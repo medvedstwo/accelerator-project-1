133cbf11db56a337070e217a272e3cd48bab9a0f
Mikhail Tereshin <medvedstwo@gmail.com>
2025-03-15 13:41:55 +0300

    feat: добавляет псевдики

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,9 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+const priceElement = document.querySelectorAll('.price-card__description');
+priceElement.forEach((el) => {
+  const priceData = el.textContent;
+  el.dataset.price = priceData;
+});
