c413e5b6dd251375b7ce39e3f2196f0f40c95470
Mikhail Tereshin <medvedstwo@gmail.com>
2025-03-17 21:59:50 +0300

    feat: добалвяет разметку свайпера и подключает свайпер

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,21 @@
 // https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
+import Swiper from 'swiper';
+import {Navigation, Pagination, Autoplay} from 'swiper/modules';
+import 'swiper/css';
 import initPrice from './utility.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initPrice();
+});
+
+const swiper = new Swiper('.swiper', {
+  modules: [Autoplay],
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  speed: 400,
+  slidesPerView: 1,
+  spaceBetween: 100,
+  loop: true,
 });
