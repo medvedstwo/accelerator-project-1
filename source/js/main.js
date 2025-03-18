ddd5efcc823e7f2988c165e88bebb26d75b9a555
Mikhail Tereshin <medvedstwo@gmail.com>
2025-03-18 22:42:46 +0300

    feat: добалвяет стилизацию свайпера

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,6 @@
 // https://swiperjs.com/get-started#installation
 import Swiper from 'swiper';
-import {Navigation, Pagination, Autoplay} from 'swiper/modules';
+import {Navigation, Autoplay} from 'swiper/modules';
 import 'swiper/css';
 import initPrice from './utility.js';
 
@@ -8,14 +8,28 @@ document.addEventListener('DOMContentLoaded', () => {
   initPrice();
 });
 
-const swiper = new Swiper('.swiper', {
-  modules: [Autoplay],
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
+const swiper = new Swiper('.my-swiper', {
+  modules: [Autoplay, Navigation],
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
   speed: 400,
   slidesPerView: 1,
   spaceBetween: 100,
   loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1366: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    }
+  },
 });
